96f61ad87176139bab40158aaa7c0fd491c396a4
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-08 16:30:07 +0000

    Route handler get

---
+++ src/app/comments/data.ts
@@ -0,0 +1,22 @@
+export const comments = [
+ {
+  id: 1,
+  text: 'This is first comment',
+ },
+ {
+  id: 2,
+  text: 'This is second comment',
+ },
+ {
+  id: 3,
+  text: 'This is third comment',
+ },
+ {
+  id: 4,
+  text: 'This is fourth comment',
+ },
+ {
+  id: 5,
+  text: 'This is fifth comment',
+ },
+];
