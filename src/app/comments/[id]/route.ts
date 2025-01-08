64ce6acb4558e2fd0c53eafdc510338a0abc75e5
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-08 16:59:20 +0000

    Route handler patch request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,8 +1,23 @@
+import { comment } from 'postcss';
 import { comments } from '../data';
 export async function GET(
  _request: Request,
  { params }: { params: { id: string } }
 ) {
- const comment = comments.find((comment) => comment.id === parseInt(params.id));
+ const comment = comments.find(
+  (comment) => comment?.id === parseInt(params?.id)
+ );
  return Response.json(comment);
+}
+export async function PATCH(
+ _request: Request,
+ { params }: { params: { id: string } }
+) {
+ const body = await _request.json();
+ const { text } = body;
+ const index = comments.findIndex(
+  (comment) => comment.id === parseInt(params.id)
+ );
+ comments[index].text = text;
+ return Response.json(comments[index]);
 }
