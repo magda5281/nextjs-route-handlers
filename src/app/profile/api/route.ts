623002fcc7c95a766003f7601c037e35324e4fd9
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-09 15:16:27 +0000

    Route handlers - headers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,13 @@
-export async function GET() {
- return new Response('Profile api data');
+import { type NextRequest } from 'next/server';
+import { headers } from 'next/headers';
+export async function GET(request: NextRequest) {
+ const requestHeaders = new Headers(request.headers);
+ console.log(requestHeaders.get('Authorization'));
+ const headerList = await headers();
+ console.log(headerList.get('Authorization'));
+
+ //to change headers return a new resopnse with the new headers
+ return new Response('<h1>Profile api data</h1>', {
+  headers: { 'Content-Type': 'text/html' },
+ });
 }
