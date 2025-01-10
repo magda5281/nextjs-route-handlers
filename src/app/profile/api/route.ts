9ffb414e7ef031d653d67526305f90a4a6860218
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-10 15:18:15 +0000

    Setting and getting cookies in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,13 +1,21 @@
 import { type NextRequest } from 'next/server';
-import { headers } from 'next/headers';
+import { headers, cookies } from 'next/headers';
 export async function GET(request: NextRequest) {
  const requestHeaders = new Headers(request.headers);
- console.log(requestHeaders.get('Authorization'));
  const headerList = await headers();
- console.log(headerList.get('Authorization'));
 
+ (await cookies()).set('resultsPerPage', '20');
+ const theme = request.cookies.get('theme');
+
+ console.log(requestHeaders.get('Authorization'));
+ console.log(headerList.get('Authorization'));
+ console.log('theme:', theme);
+ console.log('resultsPerPage:', (await cookies()).get('resultsPerPage'));
  //to change headers return a new resopnse with the new headers
  return new Response('<h1>Profile api data</h1>', {
-  headers: { 'Content-Type': 'text/html' },
+  headers: {
+   'Content-Type': 'text/html',
+   'Set-cookie': 'theme=dark',
+  },
  });
 }
