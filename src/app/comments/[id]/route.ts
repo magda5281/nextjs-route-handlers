ca97c3b95e85054656cc4eae762dac3f06aaf9a8
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-10 18:28:54 +0000

    Fixed  params types

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -2,37 +2,34 @@ import { redirect } from 'next/navigation';
 import { comments } from '../data';
 export async function GET(
  _request: Request,
- { params }: { params: { id: string } }
+ { params }: { params: Promise<{ id: string }> }
 ) {
- if (parseInt(params.id) > comments.length) {
+ const { id } = await params;
+ if (parseInt(id) > comments.length) {
   redirect('/comments');
  }
 
- const comment = comments.find(
-  (comment) => comment?.id === parseInt(params?.id)
- );
+ const comment = comments.find((comment) => comment?.id === parseInt(id));
  return Response.json(comment);
 }
 export async function PATCH(
  _request: Request,
- { params }: { params: { id: string } }
+ { params }: { params: Promise<{ id: string }> }
 ) {
+ const { id } = await params;
  const body = await _request.json();
  const { text } = body;
- const index = comments.findIndex(
-  (comment) => comment.id === parseInt(params.id)
- );
+ const index = comments.findIndex((comment) => comment.id === parseInt(id));
  comments[index].text = text;
  return Response.json(comments[index]);
 }
 
 export async function DELETE(
  _request: Request,
- { params }: { params: { id: string } }
+ { params }: { params: Promise<{ id: string }> }
 ) {
- const index = comments.findIndex(
-  (comment) => comment.id === parseInt(params.id)
- );
+ const { id } = await params;
+ const index = comments.findIndex((comment) => comment.id === parseInt(id));
 
  comments.splice(index, 1);
  return Response.json(comments[index]);
