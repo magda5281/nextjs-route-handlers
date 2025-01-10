062378d2230f0caf48edeadf5f3d6eb9a151d8f7
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-10 19:58:15 +0000

    Fixed response error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-import { redirect } from 'next/dist/server/api-utils';
-import { matchesMiddleware } from 'next/dist/shared/lib/router/router';
+// import { redirect } from 'next/dist/server/api-utils';
+
 import { NextResponse, type NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
@@ -7,7 +7,7 @@ export function middleware(request: NextRequest) {
 
  const themePreference = request.cookies.get('theme');
  if (!themePreference) {
-  return response.cookies.set('theme', 'dark');
+  response.cookies.set('theme', 'dark');
  }
  response.headers.set('custom-header', 'custom-value');
  return response;
